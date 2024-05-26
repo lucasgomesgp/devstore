7f5938f7c7be7c997a6ae5d9c9436b144c9feb6d
Lucas Gomes <lucasgomesbt@gmail.com>
2024-05-26 17:08:58 -0300

    add new interface for params

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,10 +1,14 @@
 'use client'
 
-import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
+import { ReadonlyURLSearchParams, useSearchParams } from 'next/navigation'
 
+import { Skeleton } from '@/components/skeleton'
+
+interface Params extends ReadonlyURLSearchParams {
+  q: string
+}
 export default function SearchLoading() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams() as Params
   const { q: query } = searchParams
   return (
     <div className="flex flex-col gap-4">
